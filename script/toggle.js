9163f8d66c60713e004a7ada0ba730a05c29da43
Abdul Quaiyum <abdulquaiyum@Mac.home>
2026-03-04 15:29:18 +0100

    second commit

---
+++ script/toggle.js
@@ -0,0 +1,7 @@
+document.getElementById('task-section-2')?.addEventListener('click', function(event){
+    window.location.href = './modal.html'
+})
+document.getElementById('modal-btn')?.addEventListener('click', function(event){
+   window.location.href = './index.html'
+   console.log('object');
+})
